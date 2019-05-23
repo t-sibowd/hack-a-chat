d14582c3a8c7742b5fb38d60035aa6a78dbe5c9a
Bo Tembunkiart <t-sibowd@microsoft.com>
2019-05-23 10:56:41 -0700

    really fixes url string interpolation

--- dialogs/mainDialog.js
+++ dialogs/mainDialog.js
@@ -104,7 +104,7 @@ class MainDialog extends ComponentDialog {
 
             // This is where calls to the booking AOU service or database would go.
             const fetch = require('node-fetch');
-            fetch(`http://api.nytimes.com/svc/semantic/v2/concept/name/nytd_geo/${result.location}?fields=all&api-key=4NaHSHuubILd2RT1LTH6N33w8RlY9sCE`)
+            fetch(`http://api.nytimes.com/svc/semantic/v2/concept/name/nytd_geo/${result.location}\?fields=all&api-key=4NaHSHuubILd2RT1LTH6N33w8RlY9sCE`)
                 .then(res => res.json())
                 .then(json => console.log(json));
             
